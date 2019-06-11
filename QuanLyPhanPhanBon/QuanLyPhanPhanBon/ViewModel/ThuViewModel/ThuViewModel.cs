bc841b4547b9edce5986ac9c9ce5f157c503d9c4
DuyHau <duyhau9898@gmail.com>
2019-06-12 02:22:55 +0700

    update

--- QuanLyPhanPhanBon/QuanLyPhanPhanBon/ViewModel/ThuViewModel/ThuViewModel.cs
+++ QuanLyPhanPhanBon/QuanLyPhanPhanBon/ViewModel/ThuViewModel/ThuViewModel.cs
@@ -4,6 +4,8 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
 using QuanLyPhanPhanBon.Model;
 namespace QuanLyPhanPhanBon.ViewModel.ThuViewModel
 {
@@ -11,9 +13,30 @@ namespace QuanLyPhanPhanBon.ViewModel.ThuViewModel
     {
         public ThuViewModel()
         {
+            
             getData();
-            _TongTien = "100000";OnPropertyChanged("TongTien");
+            
+            decimal Total = 0;
+            for(int i=0;i<_ListPhanBon_KH.Count;i++)
+            {
+                Total += _ListPhanBon_KH[i].Gia;
+            }
+            _TongTien = Total +"";
+            OnPropertyChanged("TongTien");
 
+        }
+        private DateTime _RecentDate;
+        public DateTime RecentDate
+        {
+            get
+            {
+                return _RecentDate;
+            }
+            set
+            {
+                _RecentDate = value;
+                OnPropertyChanged("RecentDate");
+            }
         }
         private ObservableCollection<PhanBon_KH> _ListPhanBon_KH = new ObservableCollection<PhanBon_KH>();
         public ObservableCollection<PhanBon_KH> ListPhanBon_KH
@@ -42,6 +65,52 @@ namespace QuanLyPhanPhanBon.ViewModel.ThuViewModel
                 OnPropertyChanged("TongTien");
             }
         }
+        private DateTime _SelectedDate;
+        public DateTime SelectedDate
+        {
+            get { return _SelectedDate; }
+            set
+            {
+                _SelectedDate = value;
+                OnPropertyChanged("SelectedDate");
+                
+                if(_SelectedDate!=null)
+                {
+                    for(int i=0;i<_ListPhanBon_KH.Count;i++)
+                    {
+                        if(_ListPhanBon_KH[i].NgayBan.Day==_SelectedDate.Day&& _ListPhanBon_KH[i].NgayBan.Month == _SelectedDate.Month&& _ListPhanBon_KH[i].NgayBan.Year == _SelectedDate.Year)
+                        {
+                            _ListTheoNgay.Add(_ListPhanBon_KH[i]);
+                        }
+                    }
+                    if(_ListPhanBon_KH.Count>0&&_ListTheoNgay.Count>0)
+                    {
+                        _ListPhanBon_KH.Clear();
+                        OnPropertyChanged("ListPhanBon_KH");
+                    }
+                    decimal totalDate = 0;
+                    for(int i=0;i<_ListTheoNgay.Count;i++)
+                    {
+                        _ListPhanBon_KH.Add(_ListTheoNgay[i]);
+                        totalDate += _ListTheoNgay[i].Gia;
+                    }
+                    _TongTien = totalDate + "";
+                    OnPropertyChanged("ListPhanBon_KH");
+                    
+                    return;
+                }
+            }
+        }
+        public ICommand Refresh
+        {
+            get;
+            private set;
+        }
+        public void _Refresh(object parameter)
+        {
+            getData();
+        }
+        private ObservableCollection<PhanBon_KH> _ListTheoNgay = new ObservableCollection<PhanBon_KH>();
         public void getData()
         {
             QuanLyPhanBonEntities quanLyPhanBonEntities = new QuanLyPhanBonEntities();
